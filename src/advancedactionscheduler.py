02e38e74a512fcfa488a8808666bd35b50484e38
swprojects <swprojects@gmx.com>
2018-01-09 14:03:09 +0000

    Delete group from data and list

--- src/advancedactionscheduler.py
+++ src/advancedactionscheduler.py
@@ -452,8 +452,12 @@ class Main(wx.Frame):
 
         return dlg
 
-    def GetGroupListIndex(self):
-        pass
+    def GetGroupListIndex(self, item):
+        """ only way to finding existing item in self._data by comparing TreeListItem """
+        for dataItem in self._data.keys():
+            if dataItem == item:
+                return dataItem
+        return -1
 
     def GetScheduleNames(self):
         """ return toplevel items"""
@@ -1039,7 +1043,7 @@ class Main(wx.Frame):
             elif not dlg.GetValue().replace("_","").isalnum():
                 m = "Group Name: (Name can only contain 0-9, A-Z. Underscores allowed)"
                 continue
-                
+
             self.SaveStateToUndoStack()
             newName = dlg.GetValue()
             newItem = self.groupList.AppendItemToRoot(newName)
@@ -1108,12 +1112,13 @@ class Main(wx.Frame):
         # self.OnScheduleTreeSelectionChanged()
             
     def ShowRemoveGroupDialog(self):
-        groupIdx = self.groupList.GetSelection()
+        groupIdx = self.GetGroupListIndex(self.groupList.GetSelection())
         if groupIdx is None:
             return
-            
+                    
+        itemText = self.groupList.GetItemText(groupIdx)
         dlg = wx.MessageDialog(self, 
-                               "Confirm delete group '{0}'?".format(self._data[str(groupIdx)]["columns"]["0"]), 
+                               "Confirm delete group '{0}'?".format(itemText), 
                                "Delete Group",
                                style=wx.YES_NO)
         if dlg.ShowModal() == wx.ID_NO:
@@ -1123,26 +1128,11 @@ class Main(wx.Frame):
 
         self.schedList.DeleteAllItems()
         self.groupList.DeleteItem(groupIdx)
-        del self._data[str(groupIdx)]
+        del self._data[groupIdx]
 
-        adjItems = {}
-        count = int(groupIdx)
-        for k in sorted([int(x) for x in self._data.keys()])[int(groupIdx):]:
-            adjItems[str(count)] = self._data[str(k)]
-            count += 1
-            
-        try:    
-            del self._data[str(count)] 
-        except:
-            pass # no item deleted if no items are adjusted
-            
         self._redo_stack = []
-
-        self._data.update(adjItems)
         self.WriteData()
-
-        self.OnScheduleTreeSelectionChanged()
-           
+        
     def WriteData(self):
         return
         """ write changes to data file"""
