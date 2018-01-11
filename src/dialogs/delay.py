c6c08b2ebf81e88adf481a935eda623ba978888c
swprojects <swprojects@gmx.com>
2018-01-11 17:19:08 +0000

    Cleanup delay dialog

--- src/dialogs/delay.py
+++ src/dialogs/delay.py
@@ -1,23 +1,5 @@
-"""
-Description:
-Requirements: pySerial, wxPython Phoenix
-
-glossary and of other descriptions:
-
-DMM - digital multimeter
-PSU - power supply
-SBC - single board computer
-
-INS - general instrument commands
-
-GEN - general sequence instructions
-
-"""
-
 import wx
 import base
-# from wx.lib.agw import spinctrl
-
 
 class AddDelay(wx.Dialog):
 
@@ -32,68 +14,55 @@ class AddDelay(wx.Dialog):
 
         hsizer = wx.BoxSizer(wx.HORIZONTAL)
         sbox = wx.StaticBox(panel, label="")
-        sbox_sizer = wx.StaticBoxSizer(sbox, wx.HORIZONTAL)
+        sboxSizer = wx.StaticBoxSizer(sbox, wx.HORIZONTAL)
         grid = wx.GridBagSizer(5,5)
 
         row = 0
         # row += 1 #let's start at 1, to give some space
 
-        lbl_delay = wx.StaticText(panel, label="Add custom delay:")
-        self.spin_delay = wx.SpinCtrl(panel, max=10, min=0, size=(50, -1))
-        self.spin_delay2 = wx.SpinCtrl(panel, max=59, min=0, size=(50, -1))
-        self.spin_delay.Bind(wx.EVT_SPINCTRL, self.OnSpinDelay)
-        self.spin_delay2.Bind(wx.EVT_SPINCTRL, self.OnSpinDelay)
-        self.lbl_delay = wx.StaticText(panel, label="0.0s")
-        grid.Add(lbl_delay, pos=(row,0), flag=wx.ALL|wx.EXPAND, border=5)
-        grid.Add(self.spin_delay, pos=(row,1), flag=wx.ALL|wx.EXPAND, border=5)
-        grid.Add(self.spin_delay2, pos=(row,2), flag=wx.ALL|wx.EXPAND, border=5)
-        grid.Add(self.lbl_delay, pos=(row,3), flag=wx.ALL|wx.ALIGN_BOTTOM, border=5)
+        labelDelay = wx.StaticText(panel, label="Add custom delay:")
+        self.spinDelay = wx.SpinCtrl(panel, max=60, min=0, size=(50, -1))
+        self.spinDelay2 = wx.SpinCtrl(panel, max=60, min=0, size=(50, -1))
+        self.spinDelay.Bind(wx.EVT_SPINCTRL, self.OnSpinDelay)
+        self.spinDelay2.Bind(wx.EVT_SPINCTRL, self.OnSpinDelay)
+        self.labelDelayValue = wx.StaticText(panel, label="0.0s")
+        grid.Add(labelDelay, pos=(row,0), flag=wx.ALL|wx.ALIGN_CENTER, border=5)
+        grid.Add(self.spinDelay, pos=(row,1), flag=wx.ALL|wx.ALIGN_BOTTOM, border=5)
+        grid.Add(self.spinDelay2, pos=(row,2), flag=wx.ALL|wx.ALIGN_BOTTOM, border=5)
+        grid.Add(self.labelDelayValue, pos=(row,3), flag=wx.ALL|wx.ALIGN_CENTER, border=5)
 
-        sbox_sizer.Add(grid, 1, wx.ALL|wx.EXPAND, 0)
-        sbox_sizer.AddSpacer(10)
+        sboxSizer.Add(grid, 1, wx.ALL|wx.EXPAND, 5)
+        sboxSizer.AddSpacer(10)
 
         #-----
         hsizer = wx.BoxSizer(wx.HORIZONTAL)
         hsizer.AddStretchSpacer()
-        btn_cancel = wx.Button(panel, label="Cancel", id=wx.ID_CANCEL)
-        btn_cancel.Bind(wx.EVT_BUTTON, self.OnButton)
-        btn_confirm = wx.Button(panel, label="Confirm", id=wx.ID_OK)
-        btn_confirm.Bind(wx.EVT_BUTTON, self.OnButton)
-        hsizer.Add(btn_cancel, 0, wx.ALL|wx.EXPAND, 5)
-        hsizer.Add(btn_confirm, 0, wx.ALL|wx.EXPAND, 5)
+        btnCancel = wx.Button(panel, label="Cancel", id=wx.ID_CANCEL)
+        btnCancel.Bind(wx.EVT_BUTTON, self.OnButton)
+        btnConfirm = wx.Button(panel, label="Confirm", id=wx.ID_OK)
+        btnConfirm.Bind(wx.EVT_BUTTON, self.OnButton)
+        hsizer.Add(btnCancel, 0, wx.ALL|wx.EXPAND, 5)
+        hsizer.Add(btnConfirm, 0, wx.ALL|wx.EXPAND, 5)
 
         #add to main sizer
-        sizer.Add(sbox_sizer, 0, wx.ALL|wx.EXPAND, 2)
+        sizer.Add(sboxSizer, 0, wx.ALL|wx.EXPAND, 5)
         sizer.Add(hsizer, 0, wx.ALL|wx.EXPAND, 5)
 
         panel.SetSizer(sizer)
 
         w, h = sizer.Fit(self)
-        # self.SetSize((w, h*1.5))
+        self.SetSize((w*1.5, h))
         # self.SetMinSize((w, h*1.5))
 
         # self.SetMaxSize(sizer.Fit(self))
 
-        try:
-            self.SetIcon(theme.GetIcon("psu_png"))
-        except:
-            pass
-
         self.Bind(wx.EVT_KEY_UP, self.OnKeyUp)
 
-    def OnKeyUp(self, event):
-        key = event.GetKeyCode()
-        print(event)
-        if key == wx.KEY_ESCAPE:
-            self.EndModal(wx.ID_CANCEL)
-
-    def OnSpinDelay(self, event=None):
-        s0 = self.spin_delay.GetValue()
-        s1 = self.spin_delay2.GetValue()
-
-        label = str(s0) + "." + str(s1) + "s"
-        self.lbl_delay.SetLabel(label)
-
+    def GetValue(self):
+        data = [("delay",str(self.labelDelayValue.GetLabel()))]
+        data = str(data)
+        return data
+        
     def OnButton(self, event):
         e = event.GetEventObject()
         label = e.GetLabel()
@@ -103,20 +72,27 @@ class AddDelay(wx.Dialog):
             self.EndModal(id)
         elif label == "Confirm":
             self.EndModal(id)
+            
+    def OnKeyUp(self, event):
+        key = event.GetKeyCode()
+        print(event)
+        if key == wx.KEY_ESCAPE:
+            self.EndModal(wx.ID_CANCEL)
 
+    def OnSpinDelay(self, event=None):
+        s0 = self.spinDelay.GetValue()
+        s1 = self.spinDelay2.GetValue()
+
+        label = str(s0) + "." + str(s1) + "s"
+        self.labelDelayValue.SetLabel(label)
+        
     def SetValue(self, data):
         delay = data["delay"]
 
-        self.lbl_delay.SetLabel(delay)
+        self.labelDelayValue.SetLabel(delay)
 
         #increment delay
         spin1, spin2 = delay[:-1].split(".")
 
-        self.spin_delay.SetValue(spin1)
-        self.spin_delay2.SetValue(spin2)
-
-    def GetValue(self):
-
-        data = [("delay",str(self.lbl_delay.GetLabel()))]
-        data = str(data)
-        return data
+        self.spinDelay.SetValue(spin1)
+        self.spinDelay2.SetValue(spin2)
