90e010e5bf95c9d4199743ee1195d2736efa63c7
swprojects <swprojects@gmx.com>
2018-01-25 12:11:27 +0000

    Update power dialog
    - added quick alert set
    - added option to show alert only on primary monitor
    - removed standby, hibernate option
    - renamed variables

--- src/dialogs/power.py
+++ src/dialogs/power.py
@@ -24,66 +24,73 @@ class AddPower(wx.Dialog):
                            parent,
                            title=title)
         
-        # self._variables = variables
         panel = wx.Panel(self) 
         sizer = wx.BoxSizer(wx.VERTICAL)
         
         sbox = wx.StaticBox(panel, label="")        
-        sbox_sizer = wx.StaticBoxSizer(sbox, wx.HORIZONTAL)
+        sboxSizerWarn = wx.StaticBoxSizer(sbox, wx.VERTICAL)
+        warning = ("All other schedules will be stopped")
+        lblWarn = wx.StaticText(panel, label=warning)
+        sboxSizerWarn.Add(lblWarn, 0, flag=wx.ALL|wx.ALIGN_CENTRE, border=5)
+        
+        sbox = wx.StaticBox(panel, label="")        
+        sboxSizer = wx.StaticBoxSizer(sbox, wx.VERTICAL)
         grid = wx.GridBagSizer(5,5)
-        
         row = 0
-        
-        lbl_function = wx.StaticText(panel, label="Power action:")
+        lblFunction = wx.StaticText(panel, label="Power action:")
         choices = ["Shutdown",
                    "Shutdown (Force)", 
-                   "Restart", 
-                   "Standby",
-                   "Hibernate",
+                   "Restart",
                    "Logoff"]
-        self.cbox_power = wx.ComboBox(panel, choices=choices, style=wx.CB_READONLY)
-        self.cbox_power.SetSelection(0)
-        
-        grid.Add(lbl_function, pos=(row,0), flag=wx.ALL|wx.ALIGN_CENTRE, border=5)
-        grid.Add(self.cbox_power, pos=(row,1), span=(0,2), flag=wx.ALL|wx.EXPAND, border=5)
+        self.cboxPower = wx.ComboBox(panel, choices=choices, style=wx.CB_READONLY)
+        self.cboxPower.SetSelection(0)
+        grid.Add(lblFunction, pos=(row,0), flag=wx.ALL|wx.ALIGN_CENTRE, border=5)
+        grid.Add(self.cboxPower, pos=(row,1), span=(0,2), flag=wx.ALL|wx.EXPAND, border=5)
         
         row += 1
+        lblQuick = wx.StaticText(panel, label="Quick Alert Set:")
+        choices = ["10 Seconds", 
+                   "20 Seconds", 
+                   "30 Seconds", 
+                   "1 Minute",
+                   "2 Minutes", 
+                   "3 Minutes",
+                   "5 Minutes",
+                   "10 Minutes"]
+        self.cboxQuick = wx.ComboBox(panel, choices=choices, style=wx.CB_READONLY)
+        self.cboxQuick.Bind(wx.EVT_COMBOBOX, self.OnQuickSelect)
+        grid.Add(lblQuick, pos=(row,0), flag=wx.ALL|wx.ALIGN_CENTRE, border=5)
+        grid.Add(self.cboxQuick, pos=(row,1), span=(0,2), flag=wx.ALL|wx.EXPAND, border=5)
         
-        lbl_alert = wx.StaticText(panel, label="Alert Duration (s):")
-        choices = [str(x) for x in range(15,120,15)]
-        self.cbox_alert = wx.ComboBox(panel, choices=choices)
-        self.cbox_alert.SetSelection(0)
-        
-        grid.Add(lbl_alert, pos=(row,0), flag=wx.ALL|wx.ALIGN_CENTRE, border=5)
-        grid.Add(self.cbox_alert, pos=(row,1), span=(0,2), flag=wx.ALL|wx.EXPAND, border=5)
-        
+        row += 1
+        lblAlert = wx.StaticText(panel, label="Alert Duration (s):")
+        self.spinAlert = wx.SpinCtrl(panel, min=10, max=10000, value="60")
+        grid.Add(lblAlert, pos=(row,0), flag=wx.ALL|wx.ALIGN_CENTRE, border=5)
+        grid.Add(self.spinAlert, pos=(row,1), span=(0,2), flag=wx.ALL|wx.EXPAND, border=5)
         grid.AddGrowableCol(1)
-      
-        sbox_sizer.AddSpacer(10)
-        sbox_sizer.Add(grid, 1, wx.ALL|wx.EXPAND, 2)
+        sboxSizer.Add(grid, 1, wx.ALL|wx.EXPAND, 5)
+        
+        self.chkPrimary = wx.CheckBox(panel, label="Show alert on primary display only")
+        sboxSizer.Add(self.chkPrimary, 0, wx.ALL|wx.EXPAND, 5)
+        
         #-----
         hsizer = wx.BoxSizer(wx.HORIZONTAL)
         hsizer.AddStretchSpacer()
-        btn_cancel = wx.Button(panel, label="Cancel", id=wx.ID_CANCEL)
-        btn_cancel.Bind(wx.EVT_BUTTON, self.OnButton)
-        self.btn_add = wx.Button(panel, label="Ok", id=wx.ID_OK)
-        self.btn_add.Bind(wx.EVT_BUTTON, self.OnButton)
-        # self.btn_add.Disable()
-        hsizer.Add(btn_cancel, 0, wx.ALL|wx.EXPAND, 5)
-        hsizer.Add(self.btn_add, 0, wx.ALL|wx.EXPAND, 5)
+        btnCancel = wx.Button(panel, label="Cancel", id=wx.ID_CANCEL)
+        btnCancel.Bind(wx.EVT_BUTTON, self.OnButton)
+        self.btnAdd = wx.Button(panel, label="Ok", id=wx.ID_OK)
+        self.btnAdd.Bind(wx.EVT_BUTTON, self.OnButton)
+        hsizer.Add(btnCancel, 0, wx.ALL|wx.EXPAND, 5)
+        hsizer.Add(self.btnAdd, 0, wx.ALL|wx.EXPAND, 5)
                         
         #add to main sizer
-        sizer.Add(sbox_sizer, 0, wx.ALL|wx.EXPAND, 2)
-        sizer.Add(hsizer, 0, wx.ALL|wx.EXPAND, 2)
+        sizer.Add(sboxSizerWarn, 0, wx.ALL|wx.EXPAND, 5)
+        sizer.Add(sboxSizer, 0, wx.ALL|wx.EXPAND, 5)
+        sizer.Add(hsizer, 0, wx.ALL|wx.EXPAND, 5)
         
         panel.SetSizer(sizer)  
-        
         w, h = sizer.Fit(self)
-        
-        try:
-            self.SetIcon(theme.GetIcon("psu_png"))
-        except:
-            pass
+        self.SetSize(w*1.5, h)
             
     def OnButton(self, event):
         e = event.GetEventObject()
@@ -95,29 +102,20 @@ class AddPower(wx.Dialog):
         elif label == "Ok":            
             self.EndModal(id)
 
-    def GetValue(self, data):
-        value = {}
-    
-    def SetValue(self, data):
-        name = data["action"]
-        self.cbox_power.SetValue(name)
-        alert = data["alert"]
-        self.cbox_alert.SetValue(alert)
+    def OnQuickSelect(self, event):
+        e = event.GetEventObject()
+        value = e.GetValue()
         
-    def GetValue(self):        
+        if value.endswith(" Seconds"):
+            value = value.strip(" Seconds")
+        elif value.endswith(" Minutes"):
+            value = int(value.strip(" Minutes")) * 60
+            value = str(value)
+        elif value == "1 Minute":
+            value = "60"
+        
+        self.spinAlert.SetValue(value)
+            
+        
+    def GetValue(self):
         data = []
-        data.append(("action", self.cbox_power.GetValue()))
-        try:
-            #check whether value is integer as well as nonzero value
-            if int(self.cbox_power.GetValue()) == 0:
-                value = "15"
-            else:
-                value = self.cbox_power.GetValue()
-            alert = ("alert", value)
-        except:
-            alert = ("alert", "15")
-        data.append(alert)
-        
-        data = str(data)
-                
-        return data
