afb79369328544103cc2f4d4c9d2311a781ca4e2
swprojects <swprojects@gmx.com>
2018-05-18 12:28:29 +0100

    Update Travis and add a placeholder test
    Update testing requirements to test for coverage and styling
    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,68 @@
-from distutils.core import setup
+from codecs import open
+from os import path, system
+from setuptools import setup, find_packages
+import setuptools.command.build_py
+from advancedactionscheduler.version import __version__
+
+def on_windows():
+    """Returns True if OS is Windows."""
+    return os.name == "nt"
+
+try:
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+        readme = readme_file.read()
+
+    with open(path.join(here, 'HISTORY.rst'), encoding='utf-8') as history_file:
+        history = history_file.read().replace('.. :changelog:', '')
+except FileNotFoundError:
+    readme = ""
+    history = ""
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
 
 setup(
-    # Application name:
-    name="Advanced Action Scheduler",
+    name='advanced-action-scheduler',
+    version=__version__,
+    description="Advanced Action + Task Scheduler",
+    long_description=readme + '\n\n' + history,
 
-    # Version number (initial):
-    version="0.0.1",
-
-    # Application author details:
+    # Author details
     author="Simon Wu",
-    author_email="swprojects@runbox.com",
+    author_email='swprojects@runbox.com',
 
-    # Packages
-    packages=["app"],
+    # The project's main homepage.
+    url='https://github.com/swprojects/advanced-action-scheduler',
 
-    # Include additional files into the package
-    include_package_data=True,
-
-    # Details
-    url="http://pypi.python.org/pypi/MyApplication_v010/",
-
-    #
-    # license="LICENSE.txt",
-    description="Useful towel-related stuff.",
-
-    # long_description=open("README.txt").read(),
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['docs', 'resources', 'snap', 'tests*']),
 
     # Dependent packages (distributions)
     install_requires=[
-        "flask",
+        "apscheduler",
     ],
+    
+    license="GPLv3",
+    
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # 'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Internet',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv2)',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    test_suite='tests',
+    tests_require=test_requirements,
+    entry_points={
+          'gui_scripts': [
+              # 'zippyscan = zippyipscanner.zippyipscanner:main'
+          ]
+      },
+    
 )
